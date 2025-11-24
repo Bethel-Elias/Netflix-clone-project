95398e8aa46f17494c43f110d0d04a42a0ed3f51
bethe-elias <betineba1@gmail.com>
2025-11-24 06:38:07 +0300

    netflix header and footer

--- netflix-replica/src/App.jsx
+++ netflix-replica/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Home/>
+    </div>
   )
 }
 
